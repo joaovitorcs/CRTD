21f12248a02716b65370073e726eec6bdc3d9239
joaovitorcs <jvitorcs03@gmail.com>
2021-04-07 09:23:34 -0300

    Script do relógio

---
+++ js/relogio.js
@@ -0,0 +1,31 @@
+function relogio() {
+    let data = new Date();
+    let horas = data.getHours();
+    let minutos = data.getMinutes();
+    let segundos = data.getSeconds();
+    let resultado = ``;
+
+    if (horas < 10) {
+        resultado += `0${horas}:`;
+    } else {
+        resultado += `${horas}:`;
+    }
+
+    if (minutos < 10) {
+        resultado += `0${minutos}:`;
+    } else {
+        resultado += `${minutos}:`;
+    }
+
+    if (segundos < 10) {
+        resultado += `0${segundos}`;
+    } else {
+        resultado += `${segundos}`;
+    }
+
+    document.getElementById('contador').innerHTML = resultado;
+}
+
+let iniciarRelogio = setInterval (relogio, 1000);
+
+relogio();

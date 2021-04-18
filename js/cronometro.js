149ae1a7cbad2313337cf50d05cbc3552c87b72a
joaovitorcs <jvitorcs03@gmail.com>
2021-04-17 23:16:16 -0300

    Atualização

--- js/cronometro.js
+++ js/cronometro.js
@@ -6,7 +6,45 @@ let cronometro;
 
 
 function iniciarContador() {
-    cronometro = setInterval(temporizador, milissegundos);
+    reiniciarContador();
+    cronometro = setInterval(function temporizador() {
+        segundos++;
+        if (segundos == 60) {
+            segundos = 0;
+            minutos++;
+            if (minutos == 60) {
+                minutos = 0;
+                horas++;
+            }
+        }
+    
+        let resultado = ``;
+        function formatarResultado() {
+            
+    
+            if (horas < 10) {
+                resultado += `0${horas}:`;
+            } else {
+                resultado += `${horas}:`;
+            }
+    
+            if (minutos < 10) {
+                resultado += `0${minutos}:`;
+            } else {
+                resultado += `${minutos}:`;
+            }
+    
+            if (segundos < 10) {
+                resultado += `0${segundos}`;
+            } else {
+                resultado += `${segundos}`;
+            }
+        }
+    
+        formatarResultado();
+        
+        document.getElementById('contador').innerHTML = resultado;
+    }, milissegundos);
 };
 
 function pausarContador() {
@@ -21,41 +59,11 @@ function pararContador() {
     document.getElementById('contador').innerHTML = '00:00:00';
 };
 
-function temporizador() {
-    segundos++;
-    if (segundos == 60) {
-        segundos = 0;
-        minutos++;
-        if (minutos == 60) {
-            minutos = 0;
-            horas++;
-        }
-    }
-
-    let resultado = ``;
-    function formatarResultado() {
-        
-
-        if (horas < 10) {
-            resultado += `0${horas}:`;
-        } else {
-            resultado += `${horas}:`;
-        }
-
-        if (minutos < 10) {
-            resultado += `0${minutos}:`;
-        } else {
-            resultado += `${minutos}:`;
-        }
-
-        if (segundos < 10) {
-            resultado += `0${segundos}`;
-        } else {
-            resultado += `${segundos}`;
-        }
-    }
-
-    formatarResultado();
-    
-    document.getElementById('contador').innerHTML = resultado;
+function reiniciarContador() {
+    clearInterval(cronometro);
+    horas = 0;
+    minutos = 0;
+    segundos = 0;
+    document.getElementById('contador').innerHTML = '00:00:00';
 };
+
